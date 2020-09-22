010d2bf4c30ae4e7df15943e09af46c775903686
Vicka93 <57420845+Vicka93@users.noreply.github.com>
2020-09-22 00:05:51 -0500

    updated calculate method and passed tests

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -1,11 +1,37 @@
 """
+Author: Viktoriia Denys
+Program: coupon_calculations.py
+This program accepts the amount of the purchase,the cash coupon,
+the percent coupon and it will calculate and return the total order item
+"""
+TAX = 0.06
 
 """
-
+up to $10 dollars, shipping is $5.95
+$10 and up to $30 dollars, shipping is $7.95
+$30 and up to $50 dollars, shipping is $11.95
+Shipping is free for $50 and over
+"""
 
 def calculate_price(price, cash_coupon, percent_coupon):
-    pass
+    cash_coupon_price = price - cash_coupon
+    percent_coupon_price = cash_coupon_price - (cash_coupon_price*percent_coupon/100)
+    subtotal_tax = percent_coupon_price + (percent_coupon_price*TAX)
+    shipping = 0
+
+    if percent_coupon_price < 10:
+        shipping = 5.95
+    elif percent_coupon_price < 30:
+        shipping = 7.95
+    elif percent_coupon_price < 50:
+        shipping = 11.95
+    else:
+        shipping = 0
+
+    total = subtotal_tax + shipping
+    return total
 
 
 if __name__ == '__main__':
-    print()
+    print(calculate_price(9.75, 10, 15))
+
--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -1,3 +1,11 @@
+"""
+Author: Viktoriia Denys
+Program: coupon_calculations.py
+This program accepts the amount of the purchase,the cash coupon,
+the percent coupon and it will calculate and return the total order item
+"""
+
+
 import unittest
 from store import coupon_calculations as coupon
 
@@ -14,13 +22,13 @@ class MyTestCase(unittest.TestCase):
         self.assertAlmostEqual(9.30, coupon.calculate_price(8.95, 5, 20), 2)
 
     def test_under_ten_coupon4(self):
-        self.assertAlmostEqual(5.95, coupon.calculate_price(8.00, 10, 10), 2)
+        self.assertAlmostEqual(4.04, coupon.calculate_price(8.00, 10, 10), 2)
 
     def test_under_ten_coupon5(self):
-        self.assertAlmostEqual(5.95, coupon.calculate_price(9.00, 10, 15), 2)
+        self.assertAlmostEqual(5.72, coupon.calculate_price(9.75, 10, 15), 2)
 
     def test_under_ten_coupon6(self):
-        self.assertAlmostEqual(5.95, coupon.calculate_price(10.00, 10, 20), 2)
+        self.assertAlmostEqual(3.98, coupon.calculate_price(7.68, 10, 20), 2)
 
 
 if __name__ == '__main__':
